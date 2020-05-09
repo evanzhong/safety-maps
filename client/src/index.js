15f35a05186ab966667af8f073a1460084c406dd
rishisankar <rishi.sankar@gmail.com>
2020-05-09 15:15:34 -0700

    Basic Mapbox setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+
+import Map from './Map';
 import './index.css';
 
+   
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Map />,
+  document.getElementById('map-container')
 );
