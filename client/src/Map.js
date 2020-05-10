6b472734d49efdec0918e3d80637d655df12b84a
rishisankar <rishi.sankar@gmail.com>
2020-05-10 14:01:40 -0700

    Added basic routing functionality using Mapbox Directions API

--- client/src/Map.js
+++ client/src/Map.js
@@ -9,10 +9,13 @@ class Map extends Component {
     super(props);
     this.state = {
       //on load, fix map to bay area (for now - until location services set up)
-      lng: -122.2265,
-      lat: 37.5860,
-      zoom: 8.41
-      };
+      map: null,
+      lng: -122.1702,
+      lat: 37.4821,
+      zoom: 10.94,
+    };
+    //For testing purposes - delete later
+    window.map = this;
   }
 
   componentDidMount() {
@@ -30,6 +33,118 @@ class Map extends Component {
         zoom: map.getZoom().toFixed(2)
       });
     });
+
+    this.setState({map: map});
+  }
+
+  //Labels a point on the map at the specified coords
+  labelPoint(identifier, coords) {
+    const map = this.state.map;
+    if (map.getLayer(identifier)) {
+      var data =  {
+        type: 'FeatureCollection',
+        features: [{
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'Point',
+            coordinates: coords
+          }
+        }]
+      };
+      map.getSource(identifier).setData(data);
+    } else {
+      map.addLayer({
+        id: identifier,
+        type: 'circle',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: [{
+              type: 'Feature',
+              properties: {},
+              geometry: {
+                type: 'Point',
+                coordinates: coords
+              }
+            }]
+          }
+        },
+        paint: {
+          'circle-radius': 10,
+          'circle-color': '#f30'
+        }
+      });
+    }
+  }
+
+  // render route using call to directions API
+  // For testing purposes: try in Chrome console:
+  // map.renderRoute([-122.1230542,37.4322595],[-122.15,37.45]);
+  renderRoute(start, end) {
+    const map = this.state.map;
+    var canvas = map.getCanvasContainer();
+      canvas.style.cursor = '';
+      this.labelPoint('end', end);
+      this.labelPoint('start', start);
+      
+      this.requestRoute(start, end);
+  }
+
+  // make call to directions API
+  requestRoute(start, end) {
+    const map = this.state.map;
+    // make a directions request using driving profile
+    var url = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + 
+      '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+
+    // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.onload = function() {
+      var json = JSON.parse(req.response);
+      var data = json.routes[0];
+      var route = data.geometry.coordinates;
+      var geojson = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: route
+        }
+      };
+      // render the route line
+      if (map.getSource('route')) {
+        map.getSource('route').setData(geojson);
+      } else {
+        map.addLayer({
+          id: 'route',
+          type: 'line',
+          source: {
+            type: 'geojson',
+            data: {
+              type: 'Feature',
+              properties: {},
+              geometry: {
+                type: 'LineString',
+                coordinates: route
+              }
+            }
+          },
+          layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+          },
+          paint: {
+            'line-color': '#3887be',
+            'line-width': 5,
+            'line-opacity': 0.75
+          }
+        });
+      }
+    };
+    req.send();
   }
 
   render() {
