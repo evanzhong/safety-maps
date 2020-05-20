7514bcab5d5f6e4824b4f6d6f0be566f2799244d
rishisankar <rishi.sankar@gmail.com>
2020-05-19 20:03:13 -0700

    Implemented A* pathfinding algorithm for routing based on distance

--- server/navigation/router.js
+++ server/navigation/router.js
@@ -1,3 +1,5 @@
+const PriorityQueue = require("./priority_queue");
+
 const PI = 3.1415926535;
 
 function deg2rad(deg) {
@@ -32,32 +34,77 @@ class Router {
     console.log("[Router] Data loaded!");
   }
 
-  generatePath(coord1, coord2) {
+  generatePath(start, end) {
     /*
       For now, use strings for coordinates.
       Later, implement function to find closest matching coordinate in dataset,
       then use Mapbox api to direct from start to closest matching coordinate
       Implemented using A* (will optimize for crime data in the future)
+      Algorithm described here: https://www.redblobgames.com/pathfinding/a-star/introduction.html
     */
-    return coord1;
+    
+    if (!(start in this.dataset && end in this.dataset)) {
+      return "Bad coord";
+    }
 
+    var pq = new PriorityQueue();
+
+    pq.enqueue(start, 0.0);
+
+    var came_from = {};
+    var cost_so_far = {};
+
+    cost_so_far[start] = 0.0;
+
+    while(!pq.isEmpty()) {
+      var current = pq.dequeue()["element"];
+      console.log(current);
+
+      //If reached end coord
+      if (current === end) {
+        var result = []
+        while (current !== start) {
+          result.unshift(current)
+          current = came_from[current];
+        }
+        result.unshift(start);
+        return {
+          "Route Found" : result
+        };
+      }
+
+      //Process neighbors of current coord
+      var neighbors = this.dataset[current]["adj"];
+      var i;
+      for (i = 0; i < neighbors.length; ++i) {
+        var neighbor = neighbors[i];
+        var new_cost = cost_so_far[current] + this.heuristic(current, neighbor);
+        if ((!(neighbor in cost_so_far)) || new_cost < cost_so_far[neighbor]) {
+          cost_so_far[neighbor] = new_cost;
+          pq.enqueue(neighbor, new_cost + this.heuristic(neighbor, end));
+          came_from[neighbor] = current;
+        }
+      }
+    }
+
+    return "No route";
   }
 
   // A* heuristic function - will later be optimized using crime data
   // For now, we just use Euclidean Distance in kilometers
   heuristic(coord1, coord2) {
     const earthRadiusKm = 6371.0;
-    coord1split = coord1.split(" ");
-    coord2split = coord2.split(" ");
-    c1lat = parseFloat(coord1split[0]);
-    c1long = parseFloat(coord1split[1]);
-    c2lat = parseFloat(coord2split[0]);
-    c2long = parseFloat(coord2split[1]);
+    var coord1split = coord1.split(" ");
+    var coord2split = coord2.split(" ");
+    var c1lat = parseFloat(coord1split[0]);
+    var c1long = parseFloat(coord1split[1]);
+    var c2lat = parseFloat(coord2split[0]);
+    var c2long = parseFloat(coord2split[1]);
 
-    lat1r = deg2rad(c1lat);
-    lon1r = deg2rad(c1long);
-    lat2r = deg2rad(c2lat);
-    lon2r = deg2rad(c2long);
+    var lat1r = deg2rad(c1lat);
+    var lon1r = deg2rad(c1long);
+    var lat2r = deg2rad(c2lat);
+    var lon2r = deg2rad(c2long);
 
     var u = Math.sin((lat2r - lat1r) / 2);
     var v = Math.sin((lon2r - lon1r) / 2);
