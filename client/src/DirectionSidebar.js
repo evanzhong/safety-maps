76f59f88d6cd8bb1aacababb5ec96e6df20a4be7
evazhog <evanzhong@ucla.edu>
2020-05-21 15:01:31 -0700

    patch missing prop bug

--- client/src/DirectionSidebar.js
+++ client/src/DirectionSidebar.js
@@ -75,12 +75,12 @@ class DirectionSidebar extends Component {
                 <div id="travel-mode" style={{display:`${this.state.isDisplayTrip?"block":"none"}`}}>
                     <div id="from-wrapper">
                         <FontAwesomeIcon icon={faMapPin} className="direction-icon"/> 
-                        <Geocoder map = {this.state.map} result={this.handleFrom} geocoder_identifier="geocoder_from" placeHolder="Enter your starting point"/>
+                        <Geocoder map = {this.state.map} calculate={this.sendGeo} filling={this.fillTo} from={this.state.fromFilled} to={this.state.toFilled} result={this.handleFrom} geocoder_identifier="geocoder_from" placeHolder="Enter your starting point"/>
                     </div>
                     <hr className="line"/>
                     <div id="to-wrapper">
                         <FontAwesomeIcon icon={faMapMarkerAlt} className="direction-icon"/> 
-                        <Geocoder map = {this.state.map} result={this.handleTo} geocoder_identifier="geocoder_to" placeHolder="Enter your destination"/>
+                        <Geocoder map = {this.state.map} calculate={this.sendGeo} filling={this.fillFrom} from={this.state.fromFilled} to={this.state.toFilled} result={this.handleTo} geocoder_identifier="geocoder_to" placeHolder="Enter your destination"/>
                     </div>
                 </div>
                 <div id="exercise-mode" style={{display:`${this.state.isDisplayTrip?"none":"block"}`}}>
