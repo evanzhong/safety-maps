4f543249079e6aaedc4c656cef5e148a42a22d6f
Ray Huang <rayhuang.cj@gmail.com>
2020-05-31 21:13:32 -0700

    finalize welcome popup

--- client/src/WelcomePopup.js
+++ client/src/WelcomePopup.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import Logo from "./assets/logo.png";
+
 import "./WelcomePopup.css"
 
 class Welcome extends Component {
     render() {
         return (
         <div className="container">
-                <FontAwesomeIcon icon={faTimesCircle} className="close-icon" onClick={this.props.closeContainer}/> 
+            <img src={Logo} className="logo"/> 
 
-                <h1>Sorry, it's ugly rn</h1>
-                <button onClick={this.props.doNotShowWelcome}>Do Not Show Again</button>
-                
+            <FontAwesomeIcon icon={faTimesCircle} className="close-icon" onClick={this.props.closeContainer}/>
+            <div className="text-container">
+                <p>      To encourage people to walk and exercise more, SafetyMaps will navigate you to your destination with the safest, most efficient route.     
+                </p>
+                <p>Toggle to trip or exercise depended on your goal. <br/> Leave the rest to us!
+                </p> 
+                <p>SafetyMaps is a quarter-long project by TopoPro, <br/> a collaboration between four apprentice engineers for Computer Science 97!
+                </p>
+            </div>
+            <div className="do-not-show-again-container"> 
+                <input type="radio" id="show" value="show" onChange={this.props.doNotShowWelcome}/>
+                <label for="show">
+                    Do Not Show This Again
+                </label>
+                {/* </input><button className="do-not-show-but" onClick={this.props.doNotShowWelcome}>Do Not Show Again</button> */}
+            </div>    
         </div>
         )
     }
