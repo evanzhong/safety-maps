d795a0ddb9e37123cfd37ed9ba779784b1208191
David Deng <daviddeng8@gmail.com>
2020-05-31 22:13:32 -0700

    rudimentary support for longer routes

--- server/navigation/process_directions.js
+++ server/navigation/process_directions.js
@@ -53,6 +53,22 @@ var mapbox = function process_mapbox_object(data) {
     }
 }
 
+function parse_data(error, response, body) {
+    try {
+        if (!error && response.statusCode == 200) {
+            if (response.err) {
+                res.json(errorReturn("Error in map matching API response"));
+                return;
+            }
+            var dirs = extract_directions(JSON.parse(body).matchings[0].legs[0].steps);
+            res.json(createReturn(true, data, dirs));
+        } else {
+            res.json(errorReturn("Error reaching map matching API"));
+        } 
+    } catch (error) {
+        res.json(errorReturn("Unexpected error requesting map matching API"));
+    }
+}
 
 var safetymaps = async function process_safetymaps_object(data, access_token, res) {
     // current idea - use map matching for sake of turn by turn
@@ -61,6 +77,7 @@ var safetymaps = async function process_safetymaps_object(data, access_token, re
         if (typeof(data) === 'string' || data instanceof String) {
             res.json(errorReturn(data));
         } else {
+            /*
             if (data.length > 99) {
                 res.json(createReturn(true, data, [
                     {
@@ -68,7 +85,10 @@ var safetymaps = async function process_safetymaps_object(data, access_token, re
                         "distance": 0,
                     }
                 ]));
-            }
+                return;
+            }*/
+
+            /*
             var coordStr = data[0][0] + ',' + data[0][1];
             var rad = 15;
             var radStr = "" + rad;
@@ -77,25 +97,96 @@ var safetymaps = async function process_safetymaps_object(data, access_token, re
                 radStr += ";" + rad;
             }
             var url = `https://api.mapbox.com/matching/v5/mapbox/walking/${coordStr}?access_token=${access_token}&steps=true&waypoints=0;${data.length-1}&tidy=true&radiuses=${radStr}`;
-            
-            var request = require('request');
-            request(url, function (error, response, body) {
-                try {
-                    if (!error && response.statusCode == 200) {
-                        if (response.err) {
-                            res.json(errorReturn("Error in map matching API response"));
-                            return;
-                        }
-                        var dirs = extract_directions(JSON.parse(body).matchings[0].legs[0].steps);
-                        res.json(createReturn(true, data, dirs));
-                    } else {
-                        res.json(errorReturn("Error reaching map matching API"));
-                    } 
-                } catch (error) {
-                    res.json(errorReturn("Unexpected error requesting map matching API"));
-                }
-            });
+            */
 
+
+            var request = require('request');
+
+            //index will be the element of the array that we are going to request coords up to
+
+            var end_index = 0;
+            if (data.length > 99) {
+                end_index = 98;
+            } else {
+                end_index = data.length-1;
+            }
+
+            //console.log('length')
+
+            console.log('end_index');
+            console.log(end_index);
+
+            console.log('datalength');
+            console.log(data.length);
+
+            var return_list = [];
+            function makeRequest(return_list, start_index, end_index) {
+                var coordStr = data[start_index][0] + ',' + data[start_index][1];
+                var rad = 15;
+                var radStr = "" + rad;
+                for (let i = start_index+1; i <= end_index; ++i) {
+                    coordStr = coordStr + ';' + data[i][0] + ',' + data[i][1];
+                    radStr = radStr + ";" + rad;
+                }
+                var url = `https://api.mapbox.com/matching/v5/mapbox/walking/${coordStr}?access_token=${access_token}&steps=true&waypoints=0;${end_index-start_index}&tidy=true&radiuses=${radStr}`;
+                
+                console.log('url now');
+                console.log(url);
+                request(url, function (error, response, body) {
+                    //try {
+                        /*
+                        if (error) {
+                            console.log('error');
+                        }
+                        if (response.statusCode == 200) {
+                            console.log('200');
+                        }*/
+                        if (!error && response.statusCode == 200) {
+                            if (response.err) {
+                                res.json(errorReturn("Error in map matching API response"));
+                                return;
+                            }
+                            console.log('made it once');
+                            var dirs = extract_directions(JSON.parse(body).matchings[0].legs[0].steps);
+                            //console.log(type(dirs));
+                            console.log(dirs);
+                            return_list.push(dirs);
+                            console.log("length return list");
+                            console.log(return_list.length);
+                            if (end_index === data.length - 1) {
+                                let j = 0;
+                                var combined_dirs = [];
+                                for (; j < return_list.length; ++j) {
+                                    if (j !== return_list.length - 1) {
+                                       let x =return_list[j].pop();
+                                       console.log(x);
+                                    }
+                                    combined_dirs = combined_dirs.concat(return_list[j]);
+                                }
+                                console.log(combined_dirs);
+                                res.json(createReturn(true, data, combined_dirs));
+                            }
+                            else {
+                                let newStart = end_index + 1;
+                                let newEnd = end_index + 99;
+                                if (data.length - 1 - end_index <= 99) {
+                                    newEnd = data.length - 1;
+                                }
+                                console.log("recursion");
+                                console.log("newStart" + newStart);
+                                console.log("newEnd" + newEnd);
+                                makeRequest(return_list, newStart, newEnd);
+                            }
+                            //res.json(createReturn(true, data, dirs));
+                        } else {
+                            res.json(errorReturn("Error reaching map matching API"));
+                        } 
+                    /*} catch (error) {
+                        res.json(errorReturn("Unexpected error requesting map matching API"));
+                    }*/
+                });
+            }
+            makeRequest(return_list, 0, end_index);
         }
     } catch (error) {
         console.log(error)
